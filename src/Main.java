42c3a4835f187bc01ba187dd5ac5cf623179418d
tolspy98 <rubensanchezg2001@gmail.com>
2024-12-22 01:10:09 +0100

    Initial commit Creado el metodo inicio y la lista de peliculas

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Main programa = new Main();
+        programa.inicio();
+    }
+
+    public void inicio() {
+        
+    }
+}
